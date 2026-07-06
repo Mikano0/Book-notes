13c7d1918f6bd0f7df107be991a2f03ce42b115d
Wouter Hendriks <wouter.hendriks@ziggo.nl>
2026-07-06 14:21:35 +0200

    Add edit and delete form

--- index.js
+++ index.js
@@ -104,6 +104,40 @@ app.get("/new", async (req, res) =>{
 	res.render("book-form.ejs")
 })
 
+app.get("/edit/:id", async (req, res) =>{
+	const bookId = req.params.id
+	const result = await db.query("SELECT * FROM books WHERE id = $1",[bookId])
+	const book = result.rows[0]
+	console.log(book)
+
+	res.render("book-form.ejs", {
+		book: book,
+	})
+})
+
+app.post("/edit", async (req, res) => {
+	console.log(req.body)
+	const bookId = req.body.updateItemId
+	const {title, review, rating ,} = req.body;
+	const dateread = req.body.dateread || new Date();
+
+	await db.query("UPDATE books SET title = $1, review = $2, rating = $3, date_read = $4 where id = $5",
+		[title,review,rating,dateread,bookId]
+	)
+
+	res.redirect("/")
+})
+
+
+app.post("/delete", async (req, res) => {
+	console.log(req.body)
+	const id = req.body.deleteItemId
+
+	await db.query("DELETE FROM books WHERE id = $1",[id])
+	console.log("Deleted book succesfully")
+
+	res.redirect("/")
+})
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
